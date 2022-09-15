3a27b2a2bdf7b6463ecba1775c905a69df3e0eaf
Mümin <mozdamar90@hotmail.com>
2022-09-15 12:29:44 +0300

    HW005 has been made.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,14 @@
 public class MainApp {
     public static void main(String[] args) {
         //TODO: Test your code
+
+        Wall wall=new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width ="+wall.getWidth());
+        System.out.println("height ="+wall.getHeight());
+        System.out.println("area ="+wall.getArea());
+
+        //System.out.println("area ="+wall1.getArea());
     }
 }
---
+++ src/Wall.java
@@ -0,0 +1,54 @@
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Wall() {
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+
+    }
+
+
+    public double getHeight() {
+        if (height<0){
+
+        }
+        return 0;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+
+    }
+
+    public double getArea() {
+        if (height>0&&width>0){
+            return width*height;
+        }
+        return 0;
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
